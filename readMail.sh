59b86057c17d10871e437386e68d2f85bcfbab5f
Harish K M <56727448+Harish023-git@users.noreply.github.com>
2020-01-09 12:59:58 +0530

    Update readMail.sh

--- readMail.sh
+++ readMail.sh
@@ -1,6 +1,6 @@
-openssl s_client -connect <ip>:<port>
-a01 LOGIN <username> <password>
-a02 LIST "" "*"
-a03 SELECT <mailbox>
-a04 FETCH <sequence_Numer> <data>
+openssl s_client -connect <ip>:<port>           #eg: imap.yahoo.mail.com:993
+a01 LOGIN <username> <password>                 
+a02 LIST "" "*"               
+a03 SELECT <mailbox>                            #eg: Inbox
+a04 FETCH <sequence_Numer> <data>               #eg: 1 BODY[TEXT]
 a05 logout
